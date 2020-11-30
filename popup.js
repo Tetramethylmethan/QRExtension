7d03bc22cf5aceb8f836e450192914a6a9cc2dd5
πwi <34057896+Tetramethylmethan@users.noreply.github.com>
2020-11-30 12:50:10 +0100

    Geting the url

---
+++ popup.js
@@ -0,0 +1,21 @@
+let btn = document.getElementById('btn');
+let tablink;
+chrome.tabs.getSelected(null,function(tab) {
+    tablink = tab.url;
+});
+
+btn.onclick = function() {
+    console.log(tablink);
+
+    var qrElem = document.getElementById('qrcode');
+    qrElem.innerHTML = "";
+    
+    var qrcode = new QRCode(qrElem, "", {
+        text: "tablink",
+        width: 300,
+        height: 300,
+        colorDark: "#000000",
+        colorLight: "#ffffff",
+        correctLevel: QRCode.CorrectLevel.H
+    });
+    }
